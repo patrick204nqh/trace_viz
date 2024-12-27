4b3db08d56eca67a5f3b23d16a194d7ee103de5b
Huy Nguyen <patrick204nqh@gmail.com>
2024-12-27 16:22:10 +0700

    Remove simplecov-json dependency and update coverage configuration

--- Gemfile
+++ Gemfile
@@ -11,5 +11,4 @@ group :development, :test do
   gem "rubocop", "~> 1.65"
   gem "rubocop-shopify", "~> 2.15.1"
   gem "simplecov", "~> 0.21"
-  gem "simplecov-json", "~> 0.2.3"
 end
--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 require "simplecov"
-require "simplecov-json"
 
 SimpleCov.start do
   add_filter "/spec/"
-  add_filter "/config/"
-  track_files "{app,lib}/**/*.rb"
-  SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+  track_files "{lib}/**/*.rb"
 end
 
 puts "SimpleCov coverage started..." if ENV["CI"]
